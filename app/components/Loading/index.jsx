12ea007a4963cac983172a1d678e167a31325148
vitordcode <vitordcode@gmail.com>
2023-05-25 16:04:25 -0300

    instalado react-spinner e configurado novo loader

--- app/components/Loading/index.jsx
+++ app/components/Loading/index.jsx
@@ -1,8 +1,11 @@
-
+import { InfinitySpin } from  'react-loader-spinner'
 export const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-red-500">
-      <p className="ml-2 text-white">Carregando...</p>
+    <div className="flex justify-center items-center h-screen bg-black">
+      <InfinitySpin 
+        width='200'
+        color="#f3f4f6"
+      />
     </div>
   );
 }
